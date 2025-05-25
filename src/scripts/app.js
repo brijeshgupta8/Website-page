e881d6b43f696e42ae7444c99694929b255eaba0
Evan Ayalin <siyeel04@gmail.com>
2025-05-25 18:44:54 +0800

    Finished most except footer and blog section.

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -4,6 +4,8 @@ import '../components/buttons/button.css';
 import '../components/navbar/navbar.css';
 import '../components/card/simple-card/simple-card.css';
 
+import img_1 from '../assets/img/image-1.png';
+
 import Navbar from "../components/navbar/navbar.js";
 import Button from '../components/buttons/button.js';
 
@@ -11,11 +13,36 @@ const App = function() {
     const body = document.body;
     const main = body.querySelector('main');
     const sect_header = main.querySelector('#header');
-    const header_action = sect_header.querySelector('.action'); 
-
+    
     const navbar = Navbar.render();
     body.prepend(navbar);
-
+    
+    const header_action = sect_header.querySelector('.action'); 
     const btn_header = Button({ text: 'Register' });
     btn_header.render(header_action);
+
+    const sect_feature_1 = main.querySelector('#feature-1');
+    const action_feature_1 = sect_feature_1.querySelector('.feature .action');
+    const btn_action_feature_1 = Button({ text: 'Learn More' });
+    btn_action_feature_1.render(action_feature_1); 
+
+    const sect_reviews = main.querySelector('#reviews');
+    const img_reviews = sect_reviews.querySelector('#img-review');
+    const action_reviews = sect_reviews.querySelector('.action');
+    const btn_action_reviews = Button({ 
+        text: 'Meet all customers',
+        type: 'n-icon' 
+    });
+
+    btn_action_reviews.render(action_reviews);
+    img_reviews.src = img_1;
+
+    const sect_demo = main.querySelector('#demo');
+    const action_demo = sect_demo.querySelector('.action');
+    const btn_action_demo = Button({
+        type: 'n-icon',
+        text: 'Get a Demo'
+    })
+
+    btn_action_demo.render(action_demo);
 }();
